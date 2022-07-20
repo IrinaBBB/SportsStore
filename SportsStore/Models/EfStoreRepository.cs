566536f708470165d4332d07b96eea661d9df910
IrinaBBB <balkarovairina@gmail.com>
2022-07-20 12:34:57 +0200

    add: Database configuration

---
+++ SportsStore/Models/EfStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EfStoreRepository : IStoreRepository
+    {
+        private readonly StoreDbContext _context;
+        public EfStoreRepository(StoreDbContext ctx)
+        {
+            _context = ctx;
+        }
+        public IQueryable<Product> Products => _context.Products;
+
+    }
+}
